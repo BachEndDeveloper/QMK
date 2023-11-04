bb07bbe0ada34dc7a43bb3f6c9a434a5e83acb16
Martin Furholt Bach <martin@furholt-bach.com>
2023-11-04 23:52:54 +0100

    Updated to be compatible with newest QMK firmware version.

--- gmmk/pro/rev1/ansi/keymaps/GetCoding/keymap.c
+++ gmmk/pro/rev1/ansi/keymaps/GetCoding/keymap.c
@@ -24,15 +24,15 @@ enum layers {
     MAC_FN
 };
 
-enum custom_keycodes {
-    KC_MISSION_CONTROL = SAFE_RANGE,
-    KC_LAUNCHPAD
-};
+// enum custom_keycodes {
+//     KC_MISSION_CONTROL = SAFE_RANGE,
+//     KC_LAUNCHPAD
+// };
 
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
-#define KC_MCTL KC_MISSION_CONTROL
-#define KC_LPAD KC_LAUNCHPAD
+//#define KC_MCTL KC_MISSION_CONTROL
+//#define KC_LPAD KC_LAUNCHPAD
 #define TO_WINB TO(WIN_BASE)
 #define TO_MACB TO(MAC_BASE)
 #define MO_WINF MO(WIN_FN)
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [WIN_FN] = LAYOUT(
         _______, KC_BRID, KC_BRIU, KC_TASK, KC_FLXP, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_INS,           _______,
-        _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,          KC_PSCR,
+        _______, RGB_TOG, _______, _______, _______, _______, _______,  , _______, _______, _______, _______, _______, QK_BOOT,          KC_PSCR,
         _______, _______, RGB_VAI, RGB_SAI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_PAUS,
         TO_MACB, _______, RGB_VAD, RGB_SAD, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          KC_SCRL,
         _______,          _______, RGB_HUI, RGB_HUD, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______, RGB_MOD, _______,
@@ -84,9 +84,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [MAC_BASE] = LAYOUT(
         KC_ESC,  KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_MUTE,
-        KC_GRV,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
-        KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-        KC_CAPS, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
+        KC_GRV,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_PGUP,
+        KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGDN,
+        KC_CAPS, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_HOME,
         KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
         KC_LCTL, KC_LALT,  KC_LGUI,                            KC_SPC,                             KC_RGUI, MO_MACF, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
@@ -181,20 +181,6 @@ bool led_update_user(led_t led_state) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case KC_MISSION_CONTROL:
-            if (record->event.pressed) {
-                host_consumer_send(0x29F);
-            } else {
-                host_consumer_send(0);
-            }
-            return false;  // Skip all further processing of this key
-        case KC_LAUNCHPAD:
-            if (record->event.pressed) {
-                host_consumer_send(0x2A0);
-            } else {
-                host_consumer_send(0);
-            }
-            return false;  // Skip all further processing of this key
     #ifdef RGB_MATRIX_ENABLE
     #ifdef NKRO_ENABLE
         case NK_TOGG:
@@ -264,7 +250,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 #ifdef RGB_MATRIX_ENABLE
-void rgb_matrix_indicators_user() {
+bool rgb_matrix_indicators_user() {
     if (effect_started_time > 0) {
         /* Render blinking EFFECTS */
         const uint16_t deltaTime = sync_timer_elapsed(effect_started_time);
@@ -277,7 +263,7 @@ void rgb_matrix_indicators_user() {
             if (host_keyboard_led_state().caps_lock) {
                 set_rgb_caps_leds();
             }
-            return;
+            return false;
         } else {
             /* EFFECTS duration is finished */
             effect_started_time = 0;
@@ -300,6 +286,8 @@ void rgb_matrix_indicators_user() {
     if (host_keyboard_led_state().caps_lock) {
         set_rgb_caps_leds();
     }
+    
+    return true; 
 }
 
 static void start_effects() {
